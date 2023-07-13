1fc33a8dba842b1836801830b15434394cebb5c7
BinayakJha <jha36binayak@gmail.com>
2023-07-13 11:35:58 +0545

    added new feature (saving file)

--- getdata.py
+++ getdata.py
@@ -4,21 +4,22 @@ import os
 
 pdf_name = input("Enter the name of the pdf file with .pdf: ")
 pdf = pdfplumber.open(pdf_name)
+
 tables = []
 output_format = input("Enter the output format (csv or excel): ")
 for page in pdf.pages:
     if table := page.extract_table(): # extract_table() returns None if there are no tables
+        print(f'Page {str(page.page_number)}')
         tables.append(pd.DataFrame(table[1:], columns=table[0]))
         # save all the tables in a folder called tables
         if not os.path.exists('tables'):
             os.makedirs('tables')
         for i, table in enumerate(tables):
             if output_format == "csv":
-                table.to_csv(f'tables/table{str(page.page_number)}_{str(i)}.csv', index=False)
-                print(f'tables/table{str(page.page_number)}_{str(i)}.csv')
+                table.to_csv(f'tables/table{str(page.page_number)}.csv', index=False)
             elif output_format == "excel":
-                table.to_excel(f'tables/table{str(page.page_number)}_{str(i)}.xlsx', index=False)
-                print(f'tables/table{str(page.page_number)}_{str(i)}.xlsx')
+                table.to_excel(f'tables/table{str(page.page_number)}.xlsx', index=False)
             else:
                 print("Invalid output format. Please enter either csv or excel.")
+                break
 pdf.close()
